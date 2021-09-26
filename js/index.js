881d66b4f6847c9e11cedb5281622d0c870f7115
Abdukarim01 <84226194+Abdukarim01@users.noreply.github.com>
2021-09-26 03:40:33 -0700

    sliders

--- js/index.js
+++ js/index.js
@@ -6,3 +6,23 @@ body.onscroll = ()=>{
 function longInput(){
     document.getElementById('searchInput').className = 'longinput'
 }
+let arr = ['335','670','1005','1340']
+num = -1
+function nextTo(){
+	num++
+	let div = document.getElementById('long-block');
+    div.style = `margin-left:-${arr[num]}px; transition:all 0.3s;`
+    if(num == 4){
+    	num = -1
+    }
+}
+
+let num2 = 4
+function prewTo(){
+	num2--
+	let div = document.getElementById('long-block');
+    div.style = `margin-left:-${arr[num2]}px; transition:all 0.3s;`
+    if(num2 == -1){
+    	num2 = 4
+    }
+}
