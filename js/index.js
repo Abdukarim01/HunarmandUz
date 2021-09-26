769c6961a49f28a7e0eb8144a118ae049f2fadaf
Abdukarim01 <84226194+Abdukarim01@users.noreply.github.com>
2021-09-26 08:47:37 -0700

    newsliders

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ let property = 0
 function nextTo(){
 	property+=339.5
 let div = document.getElementById('first')
-let longdiv = document.getElementById('long-block')
 div.style = `margin-left:-${property}px; transition:all 0.3s;`
 if(property == 2716){
 	property = 0}
@@ -19,7 +18,6 @@ if(property == 2716){
 function prewTo(){
 property-=339.5
 let div = document.getElementById('first')
-let longdiv = document.getElementById('long-block')
 div.style = `margin-left:-${property}px; transition:all 0.3s;`
 if(property == 0){
 	property = 3055.5}
